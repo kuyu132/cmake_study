c073cf58f065240f656f750d4f16a6eaddb12cd7
kuyuzhiqi <wangguoqun13230@gmail.com>
2024-05-19 10:43:29 +0800

    [feature]检查cpu是32或64、处理器名称

--- cmake_cookbook/chapter02/recipe_01/main.cpp
+++ cmake_cookbook/chapter02/recipe_01/main.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <cstdlib>
 
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
 std::string say_hello() {
 #ifdef IS_WINDOWS
     return std::string("Hello from Windows!");
@@ -26,8 +29,21 @@ std::string say_hello1() {
 #endif
 }
 
+std::string say_hello2() {
+    std::string arch_info(TOSTRING(ARCHITECTURE));
+    arch_info += std::string(" architecture. ");
+#ifdef IS_32_BIT_ARCH
+    return arch_info + std::string("Compiled on a 32 bit host processor.");
+#elif IS_64_BIT_ARCH
+    return arch_info + std::string("Compiled on a 64 bit host processor.");
+#else
+    return arch_info + std::string("Neither 32 nor 64 bit, puzzling ...");
+#endif
+}
+
 int main() {
     std::cout << say_hello() << std::endl;
     std::cout << say_hello1() << std::endl;
+    std::cout << say_hello2() << std::endl;
     return 0;
 }
